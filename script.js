a086a30db0c541e05f24c1d104d1802660c3fbaf
ODOGBO JOHN ADEKUNLE <ebubeodogbo@gmail.com>
2025-11-25 00:45:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
 
+  
   function addTask() {
     const taskText = taskInput.value.trim();
 
     
     if (taskText !== "") {
-      
+    
       const li = document.createElement("li");
       li.textContent = taskText;
 
