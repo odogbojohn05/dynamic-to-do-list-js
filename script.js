49bbde2296f03c113b535b104671c3f118dce00a
ODOGBO JOHN ADEKUNLE <ebubeodogbo@gmail.com>
2025-11-25 00:42:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,38 +4,36 @@ document.addEventListener("DOMContentLoaded", function () {
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
 
-  
   function addTask() {
     const taskText = taskInput.value.trim();
 
     
-    if (taskText === "") {
+    if (taskText !== "") {
+      
+      const li = document.createElement("li");
+      li.textContent = taskText;
+
+      
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.className = "remove-btn";
+
+      
+      removeButton.onclick = function () {
+        taskList.removeChild(li);
+      };
+
+      
+      li.appendChild(removeButton);
+
+      
+      taskList.appendChild(li);
+
+      
+      taskInput.value = "";
+    } else {
       alert("Please enter a task.");
-      return;
     }
-
-    
-    const li = document.createElement("li");
-    li.textContent = taskText;
-
-    
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.className = "remove-btn";
-
-    
-    removeButton.onclick = function () {
-      taskList.removeChild(li);
-    };
-
-    
-    li.appendChild(removeButton);
-
-    
-    taskList.appendChild(li);
-
-
-    taskInput.value = "";
   }
 
   
